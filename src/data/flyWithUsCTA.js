b8b5ba7bc4e27723e86f7f71f073b14bf565b444
lucascr <lcrodrigues1995@gmail.com>
2024-11-04 20:05:49 -0300

    Create flyWithUsCTA.js

---
+++ src/data/flyWithUsCTA.js
@@ -0,0 +1,15 @@
+const flyWithUsCTA = {
+  imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
+  imageAlt: "Right Rudder Marketing team on the runway",
+  headerH1: `<span class="text-primary-600">Start Your Aviation Journey</br></span>With a Discovery Flight`,
+  paragraph:
+    "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
+  buttons: [
+    {
+      name: "Schedule Today",
+      link: "/discovery-flight",
+      primary: true,
+    },
+  ],
+};
+export default flyWithUsCTA;
