70a2b79828d36f37de042814528feb4d320970d7
lucascr <lcrodrigues1995@gmail.com>
2024-11-05 17:06:06 -0300

    update pilot-training-guide

--- src/data/pilot-training-guide.js
+++ src/data/pilot-training-guide.js
@@ -211,21 +211,6 @@ const pilotTrainingGuide = {
       ],
     },
 
-    flyWithUsCTA: {
-      imagePath: "/src/assets/avel-chuklanov-QbTBCUJLqKY-unsplash(1).jpg",
-      imageAlt: "Right Rudder Marketing team on the runway",
-      headerH1: `<span class="text-accent-500">Start Your Aviation Journey</br></span>With a Discovery Flight`,
-      paragraph:
-        "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
-      buttons: [
-        {
-          name: "Schedule Today",
-          link: "/discovery-flight",
-          primary: true,
-        },
-      ],
-    },
-
     testimonials: {
       upperHeading: "The UFT Family",
       heading: "Our Students Love Us",
--- src/data/private-pilot-course.js
+++ src/data/private-pilot-course.js
@@ -88,21 +88,6 @@ const privatePilotCourse = {
       imagePath: "/src/assets/image-placeholder.jpg",
       imageAlt: "Right Rudder Marketing team on the runway",
     },
-
-    flyWithUsCTA: {
-      imagePath: "/src/assets/image-placeholder.jpg",
-      imageAlt: "Right Rudder Marketing team on the runway",
-      headerH1: `<span class="text-accent-500">Start Your Aviation Journey</br></span>With a Discovery Flight`,
-      paragraph:
-        "Doing a discovery flight is the best way to determine if being a pilot is right for you. Whether you're trying it out for fun or want to fly professionally, doing a discovery flight will allow you to see the Ft. Lauderdale and Hollywood from a new perspective.",
-      buttons: [
-        {
-          name: "Schedule Today",
-          link: "/discovery-flight",
-          primary: true,
-        },
-      ],
-    },
   },
 };
 export default privatePilotCourse;
