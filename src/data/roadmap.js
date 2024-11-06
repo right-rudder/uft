870421df03710ad72794c9ed3dd1065f6e79dfd0
lucascr <lcrodrigues1995@gmail.com>
2024-11-06 12:09:56 -0300

    place images in roadmap

--- src/data/roadmap.js
+++ src/data/roadmap.js
@@ -11,8 +11,8 @@ const Roadmap = {
           `The journey to becoming a commercial airline pilot begins with earning a Private Pilot Certificate. This foundational certification, regulated by the Federal Aviation Administration (FAA), is your introduction to aviation. Starting in a single-engine airplane, you’ll learn the fundamentals of piloting, including takeoffs, landings, navigation, and communication with air traffic control. The Private Pilot Certificate allows you to fly for personal or recreational purposes but is a critical stepping stone towards advanced training.`,
           "During this stage, you’ll focus on mastering core skills and gaining confidence in handling an aircraft independently. Private pilot training also includes ground school, where you'll study essential subjects like meteorology, aircraft systems, and regulations. This first certificate is not only a requirement but also a way to explore your passion for flying while building the foundation for future certificates and ratings.",
         ],
-        imagePath: "/src/assets/image-placeholder.webp",
-        imageAlt: "placeholder",
+        imagePath: "/src/assets/uft-student-holding-ppc.jpg",
+        imageAlt: "Universal Flight Training student holding his private pilot certificate",
       },
       {
         title: "Add Instrument Rating",
@@ -20,8 +20,8 @@ const Roadmap = {
           "After becoming a private pilot, the next step is to earn an Instrument Rating, which greatly expands your capabilities. An Instrument Rating allows you to fly under Instrument Flight Rules (IFR), enabling you to navigate and operate in low-visibility conditions, such as heavy clouds or fog. This training equips you to rely on the aircraft’s instruments rather than visual references, a skill essential for flying in diverse weather conditions.",
           "Achieving an Instrument Rating builds upon the skills learned as a private pilot by adding precision and proficiency in complex scenarios. This training helps you develop situational awareness and decision-making skills, especially when flying in controlled airspace. With an Instrument Rating, you'll be better prepared for the demands of commercial aviation, where instrument navigation is often a daily requirement.",
         ],
-        imagePath: "/src/assets/image-placeholder.webp",
-        imageAlt: "placeholder",
+        imagePath: "/src/assets/uft-instrument-proficiency-check-cover-picture.jpg",
+        imageAlt: "Universal Flight Training plane flying in low visibility",
       },
       {
         title: "Earn Commercial Pilot Certificate",
@@ -29,7 +29,7 @@ const Roadmap = {
           "Once you have the Private Pilot Certificate and Instrument Rating, the next step is to earn a Commercial Pilot Certificate. This certification requires pilots to meet higher experience requirements and to perform at a more rigorous standard. Commercial pilots are trained to a professional level of skill, enabling them to be paid for their flying services, whether in passenger transport, cargo, or specialized operations.",
           "Commercial pilot training focuses on advanced maneuvers, including emergency procedures, precision landings, and complex aircraft operations. This certification not only increases your proficiency but also opens doors to many professional opportunities in aviation. With a Commercial Pilot Certificate, you take a significant step toward fulfilling the dream of flying commercially and preparing for airline employment.",
         ],
-        imagePath: "/src/assets/image-placeholder.webp",
+        imagePath: "/src/assets/uft-commercial-pilot-training.jpg",
         imageAlt: "placeholder",
       },
       {
@@ -38,7 +38,7 @@ const Roadmap = {
           "The next step for most aspiring airline pilots is to obtain a Flight Instructor Certificate. Becoming a certificated flight instructor (CFI) allows you to build valuable flight hours while earning an income. Flight instructing is one of the most common ways for new pilots to accumulate the flight experience necessary to meet airline hiring requirements.",
           "As a CFI, you’ll refine your own skills by teaching others, which can be immensely rewarding and beneficial to your development. Instructing requires a deep understanding of aviation principles and enhances your ability to communicate complex concepts. This role allows you to immerse yourself in the aviation community, make professional connections, and gain insights into the industry as you work toward your ultimate goal.",
         ],
-        imagePath: "/src/assets/image-placeholder.webp",
+        imagePath: "/src/assets/uft-certified-flight-instructor-course-picture.webp",
         imageAlt: "placeholder",
       },
       {
@@ -47,7 +47,7 @@ const Roadmap = {
           "To qualify for positions that involve flying larger, more complex aircraft, you will need to add a Multi-Engine Rating to your Commercial Pilot Certificate. This rating allows you to operate aircraft with more than one engine, which is a requirement for most commercial airline positions. Multi-engine training focuses on handling twin-engine aircraft, especially in situations involving engine failures or other in-flight emergencies.",
           "The multi-engine rating training builds upon your existing skills, adding a new layer of complexity and decision-making. Pilots must learn to manage the additional power and speed, as well as the unique aerodynamic characteristics of multi-engine planes. Gaining this rating is crucial to advancing in your career and is typically the final step before beginning to accumulate the necessary hours to meet airline hiring standards.",
         ],
-        imagePath: "/src/assets/image-placeholder.webp",
+        imagePath: "/src/assets/uft-multi-engine-plane.jpg",
         imageAlt: "placeholder",
       },
       {
