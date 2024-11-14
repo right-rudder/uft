f3dd4ba70e44b9e66a8be712942ad3fec6eab78e
lucascr <lcrodrigues1995@gmail.com>
2024-11-14 14:40:15 -0300

    update redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://altoflight.com/",
+  site: "https://www.flyuft.com/",
   integrations: [
     mdx(),
     sitemap(),
@@ -17,6 +17,23 @@ export default defineConfig({
     partytown(),
   ],
   redirects: {
-    // View previous sitemap and Google Search Console and place redirects from old routes to new routes here
+    "/flighttrainingcourse": "/programs",
+    "/airworthiness": "/services/maintenance", // revisit
+    "/fleet": "/about/our-fleet",
+    "/crew": "/about/our-crew",
+    "/about-form": "/discovery-flight",
+    "/ground-training": "/pilot-training-guide", // revisit
+    "/book-online": "/contact",
+    "/private-pilot-qualifications": "/private-pilot-course", // revisit
+    "/copy-of-sport-pilot": "/private-pilot-course",
+    "/copy-of-event-venue": "/cats",
+    "/copy-of-services-maintenance": "/",
+    "/copy-of-introductory-flight-1": "/", // revisit
+    "/copy-of-instrument-rating-training": "/certified-flight-instructor-cfi",
+    "/copy-of-multi-engine-rating": "/commercial-pilot-training",
+    "/copy-of-commercial-pilot-training": "/instrument-rating",
+    "/copy-of-event-venue-1": "/sport-pilot",
+    "/copy-of-private-pilot-training": "/multi-engine-rating",
+    "/copy-of-introductory-flight-2": "/services/maintenance",
   },
 });
