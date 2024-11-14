e7cdc7aa79025dacbb191c0e5dd305df4d019f25
lucascr <lcrodrigues1995@gmail.com>
2024-11-14 12:52:10 -0300

    Uodate consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,21 +1,21 @@
 // CHANGE Default title
 export const SITE_TITLE =
-  "Universal Flight Training | Sarasota Flight School fo Pilot Training";
+  "Universal Flight Training | Sarasota Flight School for Pilot Training";
 
 // CHANGE default meta description -> should only be used in homepage technically, but is there for a fallback in case a page is missed
 export const SITE_DESCRIPTION =
-  "Our mission is to train professional, safe, and proficient pilots, who will one day excel in the aviation industry. When you fly with the team at Universal Flight Training, you're family. Individualized instruction means you're more than just a number. You're a future UFT aviator.";
+  "Universal Flight Training, located at Sarasota Bradenton International Airport, offers personalized flight training with a 98% checkride pass rate and over 500 graduates since 2010. Led by experienced instructors, UFT provides flexible programs tailored to each student, from Private Pilot to Certified Flight Instructor courses. Discover more about our mission, values, and modern aircraft fleet today.";
 
 //SEO tip -> keyword + city 1
 //Should have between 5-10 different localities to mix and match keywords
 //typical keywords = how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot
 // CHANGE default keywords -> should only be used in homepage technically, but is there for a fallback in case a page is missed
 export const KEYWORDS =
-  "Universal Flight Training, located at KSRQ - Sarasota Bradenton International Airport in Sarasota, Florida, how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot, flight school, pilot training";
+  "Universal Flight Training, located at SRQ - Sarasota Bradenton International Airport in Sarasota, how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot, flight school, pilot training, Universal Flight Training, Sarasota flight school, personalized flight training, flexible flight programs, Private Pilot License training, Certified Flight Instructor (CFI) Sarasota, multi-engine training Sarasota, aviation school in Florida, Cessna 172 training, discovery flights Sarasota, pilot school near me, flight training with high pass rate";
 
 export const LOCATIONS = [
   {
-    title: `Sarasota Bradenton International Airport<br><small class="text-xs -mt-3">KSRQ</small>`,
+    title: `Sarasota Bradenton International Airport<br><small class="text-xs -mt-3">SRQ</small>`,
     address: "8140 15th St E",
     city: "Sarasota",
     state: "FL",
@@ -60,11 +60,11 @@ export const COMPANY_NICKNAME = "UFT";
 export const PHONE_NUMBER = "(941) 210-4410";
 export const ADDRESS_LINE_1 = "8140 15th St E";
 export const ADDRESS_LINE_2 = "Sarasota, FL 34243";
-export const FACEBOOK_URL = "https://www.facebook.com/rightruddermarketing";
-export const INSTAGRAM_URL = "https://www.instagram.com/rightruddermarketing/";
+export const EMAIL_ADDRESS = "universalflight@gmail.com";
+export const FACEBOOK_URL = "https://www.facebook.com/fly.uft/";
+export const INSTAGRAM_URL = "https://www.instagram.com/flyuft/";
 export const LINKEDIN_URL = "http://linkedin.com/company/rightruddermarketing";
 export const TWITTER_URL = "https://twitter.com/rightruddermktg";
 export const YOUTUBE_URL = "https://www.youtube.com/@rightruddermarketing";
 export const WHATSAPP_URL = "https://wa.me/13143582100";
 export const GITHUB_URL = "https://github.com/right-rudder/";
-export const EMAIL_ADDRESS = "info@rightruddermarketing.com";
