bc2731f2b246762f4308c9dd4281bfe4ec914052
lucascr <lcrodrigues1995@gmail.com>
2024-12-05 18:50:08 -0300

    Add small benner on the index page

---
+++ src/data/partners-data.js
@@ -0,0 +1,13 @@
+const partnersData = {
+  data: {
+    partners: [
+      {
+        link: "/university-program",
+        img: "/src/assets/purdue-university-global-seeklogo.svg", // Ensure this matches the file path in your project
+        alt: "Purdue University Logo",
+      },
+    ],
+  },
+};
+
+export default partnersData;
