1b5dbf0019899d1bc3f08d7f788e226a3c405c04
Lucas <lcrodrigues1995@gmail.com>
2025-02-28 15:52:05 -0300

    Create resultD.js

---
+++ src/data/resultD.js
@@ -0,0 +1,34 @@
+import {
+  COMPANY_NAME,
+} from "../consts";
+
+const resultD = {
+  data: {
+    pageTitle: `You’re Interested but Need More Info! | ${COMPANY_NAME}`,
+    pageDescription:
+      `Not sure where to start? Our flight advisors can help you figure out the best next step for your aviation journey.`,
+    pageKeywords:
+      `quiz, flight training advice, aviation consultation, pilot career guidance, ${COMPANY_NAME}`,
+    header: {
+      imagePath: "/src/assets/aviation-consultation.jpg",
+      imageAlt: `Flight advisor speaking with a student`,
+      headerH1: "You’re Interested but Need More Info!",
+      paragraph:
+        `Not sure where to start? Our flight advisors can help you figure out the best next step for your aviation journey.`,
+      buttons: [
+        {
+          name: "Request a Call",
+          link: "/contact",
+          primary: true,
+        },
+        {
+          name: "View Our FAQs",
+          link: "/faq",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+
+export default resultD;
